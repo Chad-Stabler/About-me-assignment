0c4a8689950b1b5a9a728de94528d6572132aced
Chad Stabler <stablerpsn@gmail.com>
2022-04-19 15:06:55 -0500

    adding button event and validation log

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const showButton = document.getElementById('btn');
 const animal = document.getElementById('animalDiv');
 console.log(showButton, animal, 'Validate that we grabbed the elements');
 // set event listeners 
-
+showButton.addEventListener('click', () => {
+    console.log(' show button clicked', 'validate button is connected');
+    animal.classList.remove('hidden');
+});
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
