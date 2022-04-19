f39ce8c5e4ac8c19efcf264810d3c03820d40365
Chad Stabler <stablerpsn@gmail.com>
2022-04-19 15:17:00 -0500

    moved hidden style above button click event

--- app.js
+++ app.js
@@ -4,12 +4,13 @@
 const showButton = document.getElementById('btn');
 const animal = document.getElementById('animalDiv');
 console.log(showButton, animal, 'Validate that we grabbed the elements');
-// set event listeners 
+// set event listeners
+.hidden{ display: none;}; 
 showButton.addEventListener('click', () => {
     console.log(' show button clicked', 'validate button is connected');
     animal.classList.remove('hidden');
 });
-.hidden{ display: none;};
+
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
