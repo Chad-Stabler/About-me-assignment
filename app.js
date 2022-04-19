dc381056033a4e077a566f044ce814dfee0d110e
Chad Stabler <stablerpsn@gmail.com>
2022-04-19 15:01:10 -0500

    Grabbed HTML elements and added validation log

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // import functions
 
 // grab DOM elements
-
+const showButton = document.getElementById('btn');
+const animal = document.getElementById('animalDiv');
+console.log(showButton, animal, 'Validate that we grabbed the elements');
 // set event listeners 
+
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
